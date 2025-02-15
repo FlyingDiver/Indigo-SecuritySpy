b0266c0e410cbd26dc68a2d5a8d0169661e78922
Joe Keenan <joe@flyingdiver.com>
2025-02-15 14:38:15 -0500

    First test release.  Supports motion events and camera status (in device states).

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Indigo-SecuritySpy
-New plugin for SecuritySpy integration with Indigo
+# SecuritySpy Revived
+This is a new plugin for SecuritySpy integration with Indigo.  It is not based on any previous plugin, but is a complete rewrite.  
+
+It is designed to work with SecuritySpy 2024.0.0 and later.
+
+## Features
+- Provide Indigo event triggers for motion type events in SecuritySpy.
+- Provide visibility into SecuritySpy camera status in Indigo Devices.
+
+## Installation
+
+Install plugin as per usual.  Once installed, you will first need to create a "Security Spy Server" device.  This device
+will contain the IP address of the SecuritySpy server, and the port number that SecuritySpy is listening on.  The plugin uses 
+Zeroconf to find available SecuritySpy servers on your network, so you can select the server from a list.  
+Manual entry of IP address and port is also supported.
+
+You will need to provide the username and password for the SecuritySpy server.  This is the username and password that you 
+use to log into the SecuritySpy web interface.
+
+## Usage
+
+Once the SecuritySpy Server device is created, you can create a "SecuritySpy Camera" device for each camera that you want to monitor.
+
+You can create event triggers for any motion event, for a specific camera, or for any camera.  The event trigger will fire when the
+SecuritySpy server reports a motion event for the camera(s).  Note that SS generates a LOT of motion events, so creating generic
+motion triggers is not recommended.
+
+You can also create triggers for object detection events.  There are events for "person", "vehicle", and "animal". See the SecuritySpy
+documentation for more information on object detection.
+
+## Limitations
+
+At this time the plugin does not provide any control of the cameras or Security Spy in general.  That will be added in a future release.
+
---
+++ SecuritySpy.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -0,0 +1,274 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from os import WCONTINUED
+
+from aiohttp import ClientSession
+import asyncio
+import logging
+import json
+import os
+import time
+from datetime import timedelta
+import threading
+import queue
+
+from pysecspy.secspy_server import SecSpyServer
+
+from zeroconf import IPVersion, ServiceBrowser, ServiceStateChange, Zeroconf
+
+class Plugin(indigo.PluginBase):
+
+    def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
+        indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
+
+        pfmt = logging.Formatter('%(asctime)s.%(msecs)03d\t[%(levelname)8s] %(name)20s.%(funcName)-25s%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        self.plugin_file_handler.setFormatter(pfmt)
+        self.logLevel = int(pluginPrefs.get("logLevel", logging.DEBUG))
+        self.logger.debug(f"LogLevel = {self.logLevel}")
+        self.indigo_log_handler.setLevel(self.logLevel)
+        self.plugin_file_handler.setLevel(self.logLevel)
+
+        self.pluginId = pluginId
+        self.pluginPrefs = pluginPrefs
+        self.triggers = []
+        self.event_loop = None
+        self.async_thread = None
+
+        self.found_servers = {}     # zeroconf discovered SecuritySpy servers
+        self.spy_servers = {}       # indigo devices for SecuritySpy servers
+        self.spy_cameras = {}       # indigo devices for SecuritySpy cameras
+        self.camera_info = {}       # Current Camera Information
+
+        self.client_session = None
+
+    def startup(self):
+        self.logger.debug("startup")
+        threading.Thread(target=self.run_async_thread).start()
+        self.sleep(1)
+        ServiceBrowser(Zeroconf(ip_version=IPVersion.V4Only), ["_securityspy._tcp.local."], handlers=[self.on_service_state_change])
+        self.logger.debug("startup complete")
+
+    ##############################################################################################
+
+    def run_async_thread(self):
+        self.logger.debug("run_async_thread starting")
+
+        self.event_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.event_loop)
+        self.event_loop.run_until_complete(self.async_main())
+        self.event_loop.close()
+
+        self.logger.debug("run_async_thread exiting")
+
+    async def async_main(self):
+        self.logger.debug("async_main starting")
+
+        self.client_session = ClientSession()
+
+        while True:
+            await asyncio.sleep(0.1)
+            if self.stopThread:
+                self.logger.debug("async_main: stopping")
+                break
+
+        self.logger.debug("async_main: exiting")
+
+    def on_service_state_change(self, zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange) -> None:
+        self.logger.threaddebug(f"Service {name} of type {service_type} state changed: {state_change}: {zeroconf.get_service_info(service_type, name)}")
+
+        if state_change in [ServiceStateChange.Added, ServiceStateChange.Updated]:
+            info = zeroconf.get_service_info(service_type, name)
+            ipaddr = ".".join([f"{x}" for x in info.addresses[0]])  # ip address as string (xx.xx.xx.xx)
+            port = int(info.properties[b'portp'])
+            address = f"{ipaddr}:{port}"
+            if address not in self.found_servers:
+                self.found_servers[address] = f"{info.server}"
+                self.logger.debug(f"Adding Found Server: {address} == {info}")
+
+        elif state_change is ServiceStateChange.Removed:
+            info = zeroconf.get_service_info(service_type, name)
+            ipaddr = ".".join([f"{x}" for x in info.addresses[0]])  # address as string (xx.xx.xx.xx)
+            if ipaddr in self.found_servers:
+                del self.found_servers[ipaddr]
+
+    def closed_prefs_config_ui(self, valuesDict, userCancelled):
+        self.logger.threaddebug(f"closedPrefsConfigUi, valuesDict = {valuesDict}")
+        if not userCancelled:
+            self.logLevel = int(valuesDict.get("logLevel", logging.INFO))
+            self.logger.debug(f"LogLevel = {self.logLevel}")
+            self.indigo_log_handler.setLevel(self.logLevel)
+            self.plugin_file_handler.setLevel(self.logLevel)
+
+    ##############################################################################################
+    # Event Handlers
+    ##############################################################################################
+
+    def camera_event(self, event_info):
+        self.logger.threaddebug(f"camera_event: event_info = {json.dumps(event_info, sort_keys=True, indent=4)}")
+
+        for camera_num, camera_data in event_info.items():
+            camera_address = f"{camera_data['server_id']}:{int(camera_num):02d}"
+            self.camera_info[camera_address] = camera_data
+            self.update_camera_device(camera_address, camera_data)
+
+            self.logger.debug(f"camera_event: camera = {camera_address}, event_type = {camera_data['event_type']}, event_object = {camera_data['event_object']}")
+
+            # only process motion events
+            if camera_data['event_type'] != "motion":
+                continue
+
+            for triggerID in self.triggers:
+                trigger = indigo.triggers[triggerID]
+
+                if not ((trigger.pluginProps["camera"] == "-1") or (trigger.pluginProps["camera"] == camera_address)):
+                    self.logger.debug(f"{trigger.name}: Wrong camera for trigger, skipping")
+                    continue
+
+                if trigger.pluginTypeId == "motion_event":
+                    self.logger.debug(f"motion_event for {camera_address}")
+                    indigo.trigger.execute(trigger)
+
+                elif trigger.pluginTypeId == "person_event" and camera_data['event_object'] == "Human":
+                    self.logger.debug(f"person_event for {camera_address}")
+                    indigo.trigger.execute(trigger)
+
+                elif trigger.pluginTypeId == "vehicle_event" and camera_data['event_object'] == "Vehicle":
+                    self.logger.debug(f"vehicle_event for {camera_address}")
+                    indigo.trigger.execute(trigger)
+
+                elif trigger.pluginTypeId == "animal_event" and camera_data['event_object'] == "Animal":
+                    self.logger.debug(f"animal_event for {camera_address}")
+                    indigo.trigger.execute(trigger)
+
+    ##################
+    # Device Methods
+    ##################
+
+    def validate_device_config_ui(self, valuesDict, typeId, devId):
+        self.logger.debug(f"validateDeviceConfigUi, typeId = {typeId}, devId = {devId}, valuesDict = {valuesDict}")
+        return True, valuesDict
+
+    def device_start_comm(self, device):
+        self.logger.debug(f"{device.name}: Starting Device")
+
+        if device.deviceTypeId == 'spyServer':
+            self.spy_servers[device.id] = SecSpyServer(self.client_session,
+                                  device.pluginProps['ip_address'],
+                                  device.pluginProps['port'],
+                                  device.pluginProps['username'],
+                                  device.pluginProps['password'])
+            self.event_loop.create_task(self.spy_connect(device))
+
+        elif device.deviceTypeId == 'spyCamera':
+            self.spy_cameras[device.address] = device.id
+            if camera_info := self.camera_info.get(device.address):
+                self.update_camera_device(device.address, camera_info)
+
+    def device_stop_comm(self, device):
+        self.logger.debug(f"{device.name}: Stopping Device")
+
+        if device.deviceTypeId == 'spyServer':
+            self.event_loop.create_task(self.spy_connect(device))
+
+        elif device.deviceTypeId == 'spyCamera':
+            del self.spy_cameras[device.address]
+
+    def update_camera_device(self, camera_address, camera_info):
+        device_id = self.spy_cameras.get(camera_address)
+        device = indigo.devices.get(device_id)
+        if not device:
+            return
+
+        self.logger.debug(f"{device.name}: Updating Camera Information")
+        update_list = [{'key': k, 'value': camera_info[k]} for k in camera_info.keys() if k not in ['ptz_presets']]
+        self.logger.threaddebug(f"{device.name}: Update List: {update_list}")
+        try:
+            device.updateStatesOnServer(update_list)
+        except Exception as e:
+            self.logger.error(f"{device.name}: failed to update states: {e}")
+
+    async def spy_connect(self, device):
+        self.logger.debug(f"{device.name}: Connecting to Spy Server")
+        self.spy_servers[device.id].subscribe_websocket(self.camera_event)
+        info = await self.spy_servers[device.id].get_server_information()
+        self.logger.debug(f"{device.name}: Server: {json.dumps(info, sort_keys=True, indent=4)}")
+        update_list = [
+            {'key': "server_id", 'value': info['server_id']},
+            {'key': "server_name", 'value': info['server_name']},
+            {'key': "server_version", 'value': info['server_version']},
+        ]
+        try:
+            device.updateStatesOnServer(update_list)
+        except Exception as e:
+            self.logger.error(f"{device.name}: failed to update states: {e}")
+
+        await asyncio.sleep(5.0)    # wait for camera devices to be started before updating them
+
+        data = await self.spy_servers[device.id].update()
+        for camera_num, camera_data in data.items():
+            camera_address = f"{camera_data['server_id']}:{int(camera_num):02d}"
+            self.camera_info[camera_address] = camera_data
+            self.update_camera_device(camera_address, camera_data)
+
+    async def spy_disconnect(self, device):
+        self.logger.debug(f"{device.name}: Disconnecting from Spy Server")
+        await self.spy_servers[device.id].async_disconnect_ws()
+        del self.spy_servers[device.id]
+
+    ########################################
+    # Trigger (Event) handling
+    ########################################
+
+    def trigger_start_processing(self, trigger):
+        self.logger.debug(f"{trigger.name}: Adding Trigger")
+        assert trigger.id not in self.triggers
+        self.triggers.append(trigger.id)
+
+    def trigger_stop_processing(self, trigger):
+        self.logger.debug(f"{trigger.name}: Removing Trigger")
+        assert trigger.id in self.triggers
+        self.triggers.remove(trigger.id)
+
+    ########################################
+    # callbacks from device creation UI
+    ########################################
+
+    def get_server_list(self, filter="", valuesDict=None, typeId="", targetId=0):
+        self.logger.threaddebug(f"get_found_servers: typeId = {typeId}, targetId = {targetId}, filter = {filter}, valuesDict = {valuesDict}")
+        servers = [(k, f"{v} ({k})") for k, v in self.found_servers.items()]
+        self.logger.threaddebug(f"get_found_servers: servers = {servers}")
+        return servers
+
+    def get_camera_list(self, filter="", valuesDict=None, typeId="", targetId=0):
+        self.logger.threaddebug(f"get_found_cameras: typeId = {typeId}, targetId = {targetId}, filter = {filter}, valuesDict = {valuesDict}")
+        if "any" in filter:
+            cameras = [("-1", "- Any Camera -")]
+        elif "all" in filter:
+            cameras = [("-1", "- All Cameras -")]
+        else:
+            cameras = []
+        try:
+            for k, v in self.camera_info.items():
+                cameras.append((k, v["name"]))
+        except Exception as err:
+            pass
+        cameras.sort(key=lambda tup: tup[1])
+        self.logger.threaddebug(f"get_camera_list: cameras = {cameras}")
+        return cameras
+
+    # doesn't do anything, just needed to force other menus to dynamically refresh
+    def menu_changed(self, valuesDict=None, typeId=None, devId=None):  # noqa
+        self.logger.threaddebug(f"menuChanged: typeId = {typeId}, devId = {devId}, valuesDict = {valuesDict}")
+        return valuesDict
+
+    def menu_changed_server(self, valuesDict=None, typeId=None, devId=None):
+        self.logger.threaddebug(f"menu_changed_server: input valuesDict = {valuesDict}")
+        if address := valuesDict.get('address'):
+            parts = address.split(':')
+            valuesDict['ip_address'] = parts[0]
+            valuesDict['port'] = parts[1]
+        self.logger.threaddebug(f"menu_changed_server: return valuesDict = {valuesDict}")
+        return valuesDict
+
+    def menu_log_camera_info(self, *args):
+        self.logger.info(f"{json.dumps(self.camera_info, sort_keys=True, indent=4)}")
